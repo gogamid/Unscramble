a5a7593a3e63e6faf60f75d045d0211123335814
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-23 11:43:15 +0200

    feat: populate viewModel

--- app/src/main/java/com/example/unscramble/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/GameViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.unscramble
 
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.MAX_NO_OF_WORDS
+import com.example.unscramble.data.allWordsList
 
 class GameViewModel : ViewModel() {
   private var _score = 0
   private var _currentWordCount = 0
-  private var _currentScrambledWord = "test"
+  private lateinit var _currentScrambledWord: String
 
   val score: Int
     get() = _score
@@ -15,4 +17,44 @@ class GameViewModel : ViewModel() {
 
   val currentScrambledWord: String
     get() = _currentScrambledWord
+
+  private var wordsList: MutableList<String> = mutableListOf()
+  private lateinit var currentWord: String
+
+  /*
+  * Updates currentWord and currentScrambledWord with the next word.
+  */
+  private fun getNextWord() {
+    currentWord = allWordsList.random()
+    val tempWord = currentWord.toCharArray()
+    tempWord.shuffle()
+
+    while (String(tempWord).equals(currentWord, false)) {
+      tempWord.shuffle()
+    }
+    if (wordsList.contains(currentWord)) {
+      getNextWord()
+    } else {
+      _currentScrambledWord = String(tempWord)
+      ++_currentWordCount
+      wordsList.add(currentWord)
+    }
+  }
+
+  /*
+  * Returns true if the current word count is less than MAX_NO_OF_WORDS.
+  * Updates the next word.
+  */
+  fun nextWord(): Boolean {
+    return if (currentWordCount < MAX_NO_OF_WORDS) {
+      getNextWord()
+      true
+    } else false
+  }
+
+
+  init {
+    getNextWord()
+  }
+
 }
